900d88d18262b3d17f9ff84b1acf9c0f6185d35d
AhmedALEssam <ahmed0e0muhhamed@gmail.com>
2020-05-05 18:12:25 +0300

    fix indents,typos and errors plus code optimization

--- app/Http/Controllers/phoneController.php
+++ app/Http/Controllers/phoneController.php
@@ -2,116 +2,125 @@
 
 namespace App\Http\Controllers;
 
-// use App\Http\Controllers\Controller;
-
 use App\Classes\CountriesList;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+
 class phoneController extends Controller {
 
-    protected $countries;
-    public function __construct() {
-        $CountriesList = new CountriesList();
-        $this->countries = $CountriesList->list();
-    }
+  protected $countries;
+  public function __construct() {
+    $CountriesList = new CountriesList();
+    $this->countries = $CountriesList->list();
+  }
   
-   function index(Request $request ){
-     $number = $request->phone? $request->phone: '07700000000';
-     dump($this->getAllFormats($request->phone,'IQ')));
-     session()->flash('status', 'Done');
-     return view('welcome');
-   } 
-    
-    
+  function index(Request $request ){
+    $number = $request->phone? $request->phone: '07700000000';
+    dump($this->getAllFormats($request->phone,'IQ'));
+    dump($this->knowCountry($request->phone));
+    session()->flash('status', 'Done');
+    return view('welcome');
+  }
+
   /**
-   * ⚡️ Will send you all the number format with the country data
-   * @param {string} number accept number too and it's should have the phone number
-   * @param {string} iso accept string only, it's should have the country code like iq usa etc...
-   */
- public function getAllFormats($number, $iso ) {
+  * getAllFormats() function Will send you all the number format with the country data
+  * @param {string} number accept number too and it's should have the phone number
+  * @param {string} iso accept string only, it's should have the country code like iq usa etc...
+  */
+  public function getAllFormats($number, $iso ) {
     $obj =[];
-    $number = $this->convertNumbers2English($number); 
-    $obj['isNumber'] =$number == Str::of($number)->replaceMatches('/[^0-9]++/','');
-    $number = Str::of($number)->replaceMatches('/[^0-9]++/', ''); 
+    $tempnum = $this->convertNumbers2English($number);
+    $number = Str::of($tempnum)->replaceMatches('/[^0-9]++/', '');
+    $obj['isNumber'] =$number == $tempnum;
     $number = $this->normalize($number);
     $country = $this->findCountryByIso($iso);
     $clearNumber = $number;
     $globalK = $country['dial'] . $clearNumber;
 
-    $obj['globalZ'] = "00".$country['dial']  . $clearNumber;
+    $obj['globalZ'] = "00".$country['dial'] . $clearNumber;
     $obj['globalP'] = "+" . $country['dial'] . $clearNumber;
     $obj['globalK'] = $globalK;
     $obj['domistic'] = "0" . $clearNumber;
-    $obj['domistic2'] = $clearNumber;
+    $obj['domistic2'] = ''.$clearNumber;                   
+    //concatenating with double "single quotation marks" converts "Stringable" params to string 
+    //making it easy to read and use, you can use this method with other "Stringable" params
     $obj['format1'] = $this->format($globalK, "(NNN) NNN-NNNN");
     $obj['format2'] = $this->format($globalK, "NNN.NNN.NNNN");
     $obj['country'] = $country;
-
-    return $obj; 
+    return $obj;
   }
-
+  
   /**
-   * 🌏 Will try to know what country the number is belong to
-   * @param {string} number accept number too and it's should have the phone number
-   */
-private function knowCountry($number) {
-    $number = $this->convertNumbers2English($number);
-    $number = Str::of( $number)->replaceMatches('/[^0-9]++/', '');
-    if (`${number[0]}${number[1]}` == "00")  $number = array_shift(array_shift($number));   
-    $str = "";  
-    for ($i = 0; $i < 7; $i++) { 
-      $str = $str . $number[i]; 
-      if ($this->findCountryByDial($str)) return $this->findCountryByDial($str);
+  * knowCountry() function Will try to know what country the number is belong to
+  * @param {string} number accept number too and it's should have the phone number
+  * @return {array} of countries or {null}
+  */
+  public function knowCountry($number) {
+    $number2 = $this->convertNumbers2English($number);
+    $number = ''.Str::of( $number2)->replaceMatches('/[^0-9]++/', '');
+    if (($number[0].$number[1] == "00")||($number2[0]=='+')){
+      if($number2[0] !='+')
+        $number =''.($number >>0 ) ; 
+      $str = "";
+      $knowCountry=[];
+      for ($i = 0; $i < 7; $i++) {
+        $str = $str . $number[i];
+        if ($this->findCountryByDial($str)) 
+          array_push($knowCountry, $this->findCountryByDial($str));
+      }
+      return $knowCountry;
     }
   }
 
   /**
-   * 🧼 Will send a clean number without the extra keys
-   * @param {string} number accept number too and it's should have the phone number
-   */
-private function normalize($phoneNumber) {
+  * normalize($Number) Will return a clean $Number without the extra keys
+  * @param {string} number accept number too and it's should have the phone number
+  */  
+  private function normalize($phoneNumber) {
     return Str::of($phoneNumber)->replaceMatches(
       '/^[\+\d{1,3}\-\s]*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/',
       "$1$2$3"
     );
   }
-
+  
   /**
-   * 😎 Will give you the cool formats for the number like '(964) 781-****'
+   * format() Will give you the cool formats for the number like '(964) 781-****'
    * @param {string} number accept number too and it's should have the phone number
    */
-private function format($phoneNumber, $formatString, $options =null) {
+  private function format($phoneNumber, $formatString, $options =null) {
     // Normalize the phone number first unless not asked to do so in the options
-    if (!$options || !$options['normalize']) {
+    if (!$options || !$options['normalize'])
       $phoneNumber = $this->normalize($phoneNumber);
-    }
-    $phoneNumber = str_split($phoneNumber, 1)  ;
-     for ($i = 0;  $i < strlen($phoneNumber); $i++) {
-       $formatString = Str::of($formatString)->replaceMatches( '#N#', $phoneNumber[$i] ,1);
+    $l = strlen($phoneNumber) ;
+    $phoneNumber = str_split($phoneNumber, 1) ;
+    for ($i = 0; $i < $l ; $i++) {
+      $formatString = Str::of($formatString)->replaceMatches( '#N#', $phoneNumber[$i] ,1);
     }
     return $formatString;
   }
 
   /**
-   * 👳🏽‍♀️Arabic numbers to English
+   * converts Arabic & Persian numbers to English
    * @param {String} string Any string
    */
- private function convertNumbers2English($string) {
- return strtr($string, 
- array('۰'=>'0', '۱'=>'1', '۲'=>'2', '۳'=>'3', '۴'=>'4', '۵'=>'5', '۶'=>'6', '۷'=>'7', '۸'=>'8', '۹'=>'9',
-       '٠'=>'0', '١'=>'1', '٢'=>'2', '٣'=>'3', '٤'=>'4', '٥'=>'5', '٦'=>'6', '٧'=>'7', '٨'=>'8', '٩'=>'9'));
+  private function convertNumbers2English($string) {
+    return 
+    strtr($string,
+      array('۰'=>'0', '۱'=>'1', '۲'=>'2', '۳'=>'3', '۴'=>'4',
+            '۵'=>'5', '۶'=>'6', '۷'=>'7', '۸'=>'8', '۹'=>'9',
+            '٠'=>'0', '١'=>'1', '٢'=>'2', '٣'=>'3', '٤'=>'4',
+            '٥'=>'5', '٦'=>'6', '٧'=>'7', '٨'=>'8', '٩'=>'9')
+          );
   }
-    
-private function findCountryByIso($iso){
+  
+  private function findCountryByIso($iso){
     $this->iso = strtoupper($iso);
     return Arr::first($this->countries, function ($value, $key) use ($iso) { return $value['iso2'] == $this->iso; });
+  }
 
-
-}
-
-private function findCountryByDial($dial){
-}
- $result = Arr::where($countries , function ($value, $key)use ($dial) { return $value['dial'] == $dial; });
+  private function findCountryByDial($dial){
+    $result = Arr::where($countries , function ($value, $key)use ($dial) { return $value['dial'] == $dial; });
     return $result;
+  }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,105 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <script
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            <div class="content">
-                @if(Session::has('status'))
-                    <div class="container">
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
 
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="$('.alert').toggle()">
-                                <span aria-hidden="true">&times;</span>
-                              </button> {{ session('status') }}
-                        </div>
+<body>
+    <div class="flex-center position-ref full-height">
 
+        <div class="content">
+            @if(Session::has('status'))
+            <div class="container">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"
+                        onclick="$('.alert').toggle()">
+                        <span aria-hidden="true">&times;</span>
+                    </button> {{ session('status') }}
+                </div>
+
+            </div>
+
+            @endif
+            <form method="POST" action="/" accept-charset="UTF-8" class="form-horizontal">
+                @csrf
+                <div class=" ">
+                    <div class="form-group">
+                        <input class="form-control" required="" name="phone" type="text" value="٠٧٧٠١٢٣٤٥٦٧">
                     </div>
-
-                @endif
-                <form method="POST" action="/" accept-charset="UTF-8" class="form-horizontal">
-                    @csrf
-                    <div class=" ">
-                        <div class="form-group">
-                            <input class="form-control" required="" name="phone" type="text" value="٠٧٧٠١٢٣٤٥٦٧">
-                        </div>
 
                     <div class="modal-footer">
                         <input class="btn btn-primary" type="submit" value="ارسال">
                     </div>
-                    </form>
-            </div>
+            </form>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
